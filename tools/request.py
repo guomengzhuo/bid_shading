8a55b80440fea07f04c3e9ce50ef6830eb3aa86d
biglongyuan <biglongyuan@tencent.com>
2023-01-16 16:06:02 +0800

    1

--- tools/request.py
+++ tools/request.py
@@ -12,31 +12,36 @@
 import requests
 
 
-def predict(mdate, media_app_id, position_id, ecpm, market_price=0):
+def predict(mdate, media_app_id, position_id, ecpm_min, ecpm_max, gap, market_price=0):
 
-    data = "{\"inputs\": {" + f"\"mdate\": [{mdate}],\"media_app_id\": [{media_app_id}], " \
-                              f"\"position_id\": [{position_id}], \"ecpm\": [{ecpm}], " \
-                              f"\"market_price\": [{market_price}]" + "}}"
-
-    # data = """{"inputs": {"mdate": [11],
-    #                "media_app_id": [2001],
-    #                "position_id": [30020],
-    #                "ecpm": [11],
-    #                "market_price": [100]}
-    #     }"""
-
+    pre_result = {}
     url = "http://localhost:8501/v1/models/dlf:predict"
-    r = requests.post(url, data=data)
+    while ecpm_min <= ecpm_max:
+        data = "{\"inputs\": {" + f"\"mdate\": [{mdate}],\"media_app_id\": [{media_app_id}], " \
+                                  f"\"position_id\": [{position_id}], \"ecpm\": [{ecpm_min}], " \
+                                  f"\"market_price\": [{market_price}]" + "}}"
 
-    print(r.json())
-    return r.json().get('outputs').get('final_dead_rate')[0]
+        # data = """{"inputs": {"mdate": [11],
+        #                "media_app_id": [2001],
+        #                "position_id": [30020],
+        #                "ecpm": [11],
+        #                "market_price": [100]}
+        #     }"""
+        r = requests.post(url, data=data)
+        win_rate = r.json().get('outputs').get('final_dead_rate')[0]
+        pre_result[str(ecpm_min)] = str(win_rate)
+        ecpm_min += gap
+
+    return {f"{media_app_id}_{position_id}": pre_result}
 
 
 if __name__ == '__main__':
     mdate = 11
     media_app_id = 2001
     position_id = 30020
-    ecpm = 11
+    ecpm_min = 0
+    ecpm_max = 200
+    gap = 10
     market_price = 100
-    final_dead_rate = predict(mdate, media_app_id, position_id, ecpm, market_price)
+    final_dead_rate = predict(mdate, media_app_id, position_id, ecpm_min, ecpm_max, gap, market_price)
     print(f"final_dead_rate:{final_dead_rate}")
